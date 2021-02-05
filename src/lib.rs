0e333223a64c7a499ea23ac72eeb78d6bbf38f22
Max Bond <max.o.bond@gmail.com>
2021-02-05 16:27:16 -0700

    Implemented addition & subtraction using complements
    I rewrote the existing single-digit addition function in order to better
    support implementing subtraction using the method of complements, which allows
    us to use addition to implement subtraction.
    
    I also fixed two small bugs in the complements() function. Unfortunately,
    due to the large amount of changes, I could not easily pare these changes out
    using git -p for an atomic commit. One bug was caused by a typo, causing me
    to use the wrong variable & to produce the wrong output on numbers with low-order
    0s. The other was a failure to prevent out-of-bounds access in a while loop, causing
    a crash on single-digit numbers.

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,18 @@
 pub mod util;
 pub mod iter;
 
-use std::collections::{VecDeque, vec_deque::Iter};
+use std::collections::{VecDeque, vec_deque::Iter as VecIter, vec_deque::IntoIter as VecIntoIter};
 use std::convert::TryFrom;
-use std::ops::{Shl, Shr, Range, Neg};
-use std::iter::Map;
+use std::ops::{Shl, Shr, Range, Neg, Add, Sub};
+use std::iter::{Map, Zip};
+use std::cmp::Ordering;
 
 use util::*;
 use iter::*;
 
 pub type Digit = u8;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Number {
     digits: VecDeque<Digit>,
     power: isize,
@@ -186,20 +187,19 @@ impl Number {
         Ok((power - self.power) as usize)
     }
 
-    // TODO better name
-    pub fn arithmetic_setup(&mut self, power: isize) -> Result<usize, usize> {
+    fn get_power(&mut self, power: isize) -> usize {
         match self.power_idx(power) {
-            Ok(idx) => Ok(idx), // We have a digit in this power - return it
+            Ok(idx) => idx, // We have a digit in this power - return it
             Err(PowerIndexError::InsufficientDigitsLow(needed)) => {
                 self.pad_digits_low(needed);
-                Err(0)
+                0
                 // We didn't have a digit in this power & needed more low-order
                 // digits - the 0th digit is now at this power
             },
             Err(PowerIndexError::InsufficientDigitsHigh(needed)) => {
                 let idx = (self.digits.len() - 1) + needed;
                 self.pad_digits_high(needed);
-                Err(idx)
+                idx
                 // We didn't have a digit in this power & needed more high-order
                 // digits - the index is the number of digits we added past the
                 // previous last digit
@@ -207,37 +207,69 @@ impl Number {
         }
     }
 
-    pub fn add_digit(&mut self, digit: Digit, power: isize) {
-        // TODO add asserts
-        if self.digits.len() == 0 {
-            self.push_high(digit);
-            self.power = power;
-            return;
-        }
-        
-        match self.arithmetic_setup(power) {
-            Err(idx) => self.digits[idx] = digit, // We had to allocate - eg, digit is 0
-            Ok(mut idx) => {
-                let first_digit = self.digits[idx];
-                let (new_digit, mut carry) = self.base.addition_lookup(first_digit, digit);
-                self.digits[idx] = new_digit;
-                idx += 1;
-                while idx < self.digits.len() && carry != 0 {
-                    // TODO when destucturing assignment is released, change this :(
-                    let (new_digit, new_carry) = self.base.addition_lookup(carry, self.digits[idx]);
-                    carry = new_carry;
-                    self.digits[idx] = new_digit;
-                    idx += 1;
-                }
-                if carry != 0 {
-                    self.push_high(carry);
-                }
+    fn add_digit(&mut self, digit: Digit, power: isize, propogate_carry: bool) {
+        let mut carry = digit as usize;
+        for idx in self.get_power(power)..self.digits.len() {
+            let sum = (self.digits[idx] as usize) + carry;
+            carry = sum / self.base;
+            let new_digit = sum - (carry * self.base);
+            self.digits[idx] = new_digit as Digit;
+            if carry == 0 {
+                break;
             }
         }
+        if carry != 0 && propogate_carry {
+            self.push_high(carry as Digit)
+        }
     }
-//     pub fn sub_digit(&mut self, digit: Digit, power: usize) {
-// 
-//     }
+
+    fn match_digits_and_powers(&mut self, other: &mut Self) {
+        // Match powers & number of digits - complement only works w/
+        // same number of digits, same number of digits only meaningful
+        // w/ same power
+        // TODO overflow issues
+        if self.power > other.power {
+            self.pad_digits_low(isize::abs(self.power - other.power) as usize)
+        } else if other.power > self.power {
+            other.pad_digits_low(isize::abs(other.power - self.power) as usize)
+        }
+        if self.digits.len() > other.digits.len() {
+            other.pad_digits_high(self.digits.len() - other.digits.len())
+        } else if other.digits.len() > self.digits.len() {
+            self.pad_digits_high(other.digits.len() - self.digits.len())
+        }
+    }
+
+    /// Must be equal in length & power
+    fn compare_abs(&mut self, other: &mut Self) -> Ordering {
+        let mut gt_idx = self.digits.len()-1;
+        while gt_idx > 0 && (self.digits[gt_idx] == other.digits[gt_idx]) {
+            gt_idx -= 1;
+        }
+        if self.digits[gt_idx] == other.digits[gt_idx] {
+            Ordering::Equal
+        } else if self.digits[gt_idx] > other.digits[gt_idx] {
+            Ordering::Greater
+        } else {
+            Ordering::Less
+        }
+    }
+
+    /// Takes the radix complement of the number
+    pub fn complement(&mut self) {
+        if self.digits.len() == 0 {
+            return;
+        }
+        let mut nonzero_idx = 0;
+        while nonzero_idx < self.digits.len() && self.digits[nonzero_idx] == 0 {
+            nonzero_idx += 1;
+        }
+        for idx in nonzero_idx..self.digits.len() {
+            let carry = if idx == nonzero_idx { 0 } else { 1 };
+            let new_digit = (self.base - ((self.digits[idx] + carry) as usize)) % self.base;
+            self.digits[idx] = new_digit as Digit;
+        }
+    }
 
     pub fn negate(&mut self) {
         self.sign = match self.sign {
@@ -248,7 +280,7 @@ impl Number {
 
     /// Iteration
 
-    pub fn digit_iter(&self) -> Map<Iter<'_, Digit>, fn(&u8) -> u8> {
+    pub fn digit_iter(&self) -> Map<VecIter<'_, Digit>, fn(&u8) -> u8> {
         self.digits.iter().map(|d| *d)
     }
 
@@ -270,7 +302,6 @@ impl Number {
         }
         let mut n = 0;
         for (digit, power) in self.digit_and_power_iter() {
-            // TODO make power i32?
             n += self.base.pow(power as u32) * (digit as usize);
         }
         if self.negative() {
@@ -297,6 +328,134 @@ impl Number {
         }
         n
     }
+
+    pub fn as_u128(&self) -> u128 {
+        if !self.is_integer() {
+            panic!("Unable to convert to u128: number is not an integer");
+        }
+        if !self.positive() {
+            panic!("Unable to convert to u128: number is negative");
+        }
+        let mut n = 0;
+        let base = self.base as u128;
+        for (digit, power) in self.digit_and_power_iter() {
+            n += base.pow(power as u32) * (digit as u128);
+        }
+        n
+    }
+
+    pub fn as_i128(&self) -> i128 {
+        if !self.is_integer() {
+            panic!("Unable to convert to i128: number is not an integer");
+        }
+        let mut n = 0;
+        let base = self.base as u128;
+        for (digit, power) in self.digit_and_power_iter() {
+            n += base.pow(power as u32) * (digit as u128);
+        }
+        if self.negative() {
+            if n == (i128::MAX as u128) + 1 {
+                i128::MIN
+            } else {
+                -(n as i128)
+            }
+        } else {
+            n as i128
+        }
+    }
+}
+
+impl IntoIterator for Number {
+    type Item = (Digit, isize);
+
+    type IntoIter = Zip<VecIntoIter<Digit>, Range<isize>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let powers = self.power_iter();
+        self.digits.into_iter().zip(powers)
+    }
+}
+
+impl Neg for Number {
+    type Output = Self;
+    fn neg(mut self) -> Self::Output {
+        self.negate();
+        self
+    }
+}
+
+impl Add<Number> for Number {
+    type Output = Self;
+
+    fn add(mut self, mut rhs: Number) -> Self::Output {
+        if self.sign == rhs.sign {
+            for (digit, power) in rhs {
+                self.add_digit(digit, power, true);
+            }
+        } else {
+            self.match_digits_and_powers(&mut rhs);
+            let overflow;
+            match self.compare_abs(&mut rhs) {
+                Ordering::Equal => {
+                    self.digits.truncate(0);
+                    return self;
+                }
+                Ordering::Greater => { overflow = false; }
+                Ordering::Less => { overflow = true; }
+            }
+            if self.negative() { self.complement() } else { rhs.complement() }
+            for (digit, power) in rhs {
+                self.add_digit(digit, power, false);
+            }
+            if overflow {
+                self.negate();
+            }
+            if self.negative() {
+                self.complement()
+            }
+        }
+        self
+    }
+}
+impl Add<usize> for Number {
+    type Output = Self;
+
+    fn add(self, rhs: usize) -> Self::Output {
+        let rhs = Number::from_usize(self.base, rhs);
+        self + rhs
+    }
+}
+impl Add<isize> for Number {
+    type Output = Self;
+
+    fn add(self, rhs: isize) -> Self::Output {
+        let rhs = Number::from_isize(self.base, rhs);
+        self + rhs
+    }
+}
+
+impl Sub<Number> for Number {
+    type Output = Self;
+
+    fn sub(self, rhs: Number) -> Self::Output {
+        self + -rhs
+    }
+}
+impl Sub<usize> for Number {
+    type Output = Self;
+
+    fn sub(self, rhs: usize) -> Self::Output {
+        let rhs = Number::from_usize(self.base, rhs);
+        self + -rhs
+    }
+}
+impl Sub<isize> for Number {
+    type Output = Self;
+
+    fn sub(self, rhs: isize) -> Self::Output {
+        let rhs = Number::from_isize(self.base, rhs);
+        self + -rhs
+    }
 }
 
 impl Shl<usize> for Number {
@@ -316,13 +475,6 @@ impl Shr<usize> for Number {
         self
     }
 }
-impl Neg for Number {
-    type Output = Self;
-    fn neg(mut self) -> Self::Output {
-        self.negate();
-        self
-    }
-}
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub enum Sign {
@@ -338,6 +490,23 @@ enum PowerIndexError {
 #[cfg(test)]
 pub mod test {
     use super::*;
+
+//     #[test]
+//     fn testcase() {
+// 
+// 
+//         let mut n = Number::from_isize(10, 10);
+//         n = n + -11isize;
+//         assert_eq!(n.as_isize(), -1);
+// 
+//         let mut n = Number::from_isize(10, -10);
+//         n = n + 11isize;
+//         assert_eq!(n.as_isize(), 1);
+// 
+//         let mut n = Number::from_isize(10, -100);
+//         n = n + 1isize;
+//         assert_eq!(n.as_isize(), -99);
+//     }
 
     /// Number Vector API
 
@@ -534,6 +703,50 @@ pub mod test {
         assert_eq!(n.as_usize(), 8799997);
     }
 
+
+    #[test]
+    fn add_positive_single_digit_number_to_many_digit_positive_number() {
+        let mut n = Number::from_usize(10, 100);
+        n = n + 1isize;
+        assert_eq!(n.as_usize(), 101);
+    }
+
+    #[test]
+    fn add_negative_single_digit_number_to_many_digit_positive_number() {
+
+        let mut n = Number::from_usize(10, 100);
+        n = n + -1isize;
+        assert_eq!(n.as_usize(), 99);
+    }
+
+    #[test]
+    fn add_negative_single_digit_number_positive_single_digit_number() {
+        let mut n = Number::from_usize(10, 1);
+        n = n + -1isize;
+        assert_eq!(n.as_usize(), 0);
+    }
+
+    #[test]
+    fn add_negative_single_digit_number_negative_single_digit_number() {
+        let mut n = Number::from_isize(10, -1);
+        n = n + -1isize;
+        assert_eq!(n.as_isize(), -2);
+    }
+
+    #[test]
+    fn add_to_negative_number_with_change_of_sign() {
+        let mut n = Number::from_isize(10, -9);
+        n = n + 10isize;
+        assert_eq!(n.as_isize(), 1)
+    }
+    
+    #[test]
+    fn add_to_positive_number_with_change_of_sign() {
+        let mut n = Number::from_isize(10, 9);
+        n = n + -10isize;
+        assert_eq!(n.as_isize(), -1)
+    }
+
     /// Conversion
 
     #[test]
@@ -612,33 +825,48 @@ pub mod property_tests {
             prop_assert!(n.as_isize() == number);
         }
 
-//         #[test]
-//         fn arbitrary_complements() {
-//         }
+        #[test]
+        fn arbitrary_complements((base, number) in (arbitrary_base(), any::<usize>())) {
+            let mut n = Number::from_usize(base, number);
+            n.complement();
+            let digits = ((number as f64).log(base as f64).floor() + 1.0) as u32;
+            let expected = (base as u128).pow(digits) - (number as u128);
+            // No guarentee that the complement of a usize will fit in a usize
+            prop_assert!(n.as_u128() == expected)
+        }
+
+        #[test]
+        fn arbitrary_integer_addition((base, a, b) in (arbitrary_base(), any::<isize>(), any::<isize>())) {
+            let expected = (a as i128) + (b as i128);
+            let mut a = Number::from_isize(base, a);
+            let b = Number::from_isize(base, b);
+            a = a + b;
+            prop_assert!(a.as_i128() == expected);
+        }
+
+        #[test]
+        fn any_integer_plus_its_negative_is_zero(number in arbitray_integer()) {
+            let mut a = number;
+            let mut b = a.clone();
+            b.negate();
+            a = a + b;
+            prop_assert!(a.as_isize() == 0);
+        }
+
+        #[test]
+        fn sum_of_many_integers((base, numbers) in (arbitrary_base(), proptest::collection::vec(any::<isize>(), 3..10))) {
+            let mut native_accumulator = 0i128;
+            let mut number_accumulator = Number::from_usize(base, 0);
+            for number in numbers {
+                native_accumulator += number as i128;
+                number_accumulator = number_accumulator + Number::from_isize(base, number);
+            }
+            prop_assert!(number_accumulator.as_i128() == native_accumulator);
+        }
 
     );
 
     /// Strategies
-
-//     // TODO better name
-//     fn base_digit_power_with_power_gt_0() -> impl Strategy<Value=(Base, u8, isize)> {
-//         (
-//             any::<u8>().prop_filter(
-//                 "Base must be > 1",
-//                 |base| *base > 1
-//             ),
-//             any::<u8>(),
-//             any::<u8>()
-//         )
-//         .prop_filter(
-//             "Do not cause overflow with exponentiation",
-//             |(base, _, power)| (*power as usize) < ((isize::MAX as f64).log(*base as f64)) as usize
-//         )
-//         .prop_filter(
-//             "Do not cause overflow with multiplcation",
-//             |(base, digit, power)| (*digit as isize) < isize::MAX / (*base as isize).pow(*power as u32)
-//         )
-//     }
 
     fn arbitrary_base() -> impl Strategy<Value=usize> {
         any::<u8>()
@@ -652,5 +880,10 @@ pub mod property_tests {
     fn arbitray_positive_integer() -> impl Strategy<Value=Number> {
         (arbitrary_base(), any::<usize>())
         .prop_map(|(base, number)| Number::from_usize(base, number))
+    }
+
+    fn arbitray_integer() -> impl Strategy<Value=Number> {
+        (arbitrary_base(), any::<isize>())
+        .prop_map(|(base, number)| Number::from_isize(base, number))
     }
 }
